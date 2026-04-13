fc89f42e8856f26915f0fbd38bb210ee378d4d68
Tim Spinney <116597436+tim-spinney@users.noreply.github.com>
2026-04-13 16:31:58 -0700

    added model for shopping cart

---
+++ lib/shopping_cart.dart
@@ -0,0 +1,29 @@
+
+import 'food_item.dart';
+import 'shopping_cart_item.dart';
+
+class ShoppingCart {
+  final _items = <ShoppingCartItem>[];
+
+  int get numDistinctItems => _items.length;
+
+  int get totalQuantities => _items.fold(
+    0,
+      (total, item) => total + item.quantity
+  );
+
+  addItem(FoodItem foodItem, int quantity) {
+    final existingItemIndex = _items.indexWhere(
+            (item) => item.foodItem == foodItem
+    );
+    if(existingItemIndex == -1) {
+      _items.add(ShoppingCartItem(foodItem, quantity));
+    } else {
+      _items[existingItemIndex] = ShoppingCartItem(foodItem, _items[existingItemIndex].quantity + quantity);
+    }
+  }
+
+  removeItem(FoodItem foodItem) {
+    _items.removeWhere((item) => item.foodItem == foodItem);
+  }
+}
---
+++ lib/shopping_cart_item.dart
@@ -0,0 +1,9 @@
+
+import 'package:pizza_app/food_item.dart';
+
+class ShoppingCartItem {
+  final FoodItem foodItem;
+  final int quantity;
+
+  ShoppingCartItem(this.foodItem, this.quantity);
+}
